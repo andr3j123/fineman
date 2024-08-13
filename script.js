7b3935916d7dadce72c70b3104f21cb62afe3e60
Andrej <andrejkov007@gmail.com>
2024-08-13 21:33:46 +0200

    changes
    When you hover over username instead of only changing text it now changes color too

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-// Checks if DOM is loaded and will execute function that changes text on hover.
+// Checks if DOM is loaded and will execute function that changes text and color on hover.
 
 document.addEventListener("DOMContentLoaded", () => {
+  const logOut = document.getElementById("userName");
+  const logOutText = document.getElementById("userName").innerHTML;
 
-    const logOut = document.getElementById("userName");
-    const logOutText = document.getElementById("userName").innerHTML;
-
-    logOut.addEventListener("mouseover", (event) => {
-        event.target.innerHTML = "Log Out?";
-    });
-    logOut.addEventListener("mouseout", (event) => {
-        event.target.innerHTML = logOutText;
-    });
-
-})
+  logOut.addEventListener("mouseover", (event) => {
+    event.target.innerHTML = "Log Out?";
+    event.target.style.color = "red";
+  });
+  logOut.addEventListener("mouseout", (event) => {
+    event.target.innerHTML = logOutText;
+    event.target.style.color = "";
+  });
+});
 
 // Some functions that will redirect user to some pages.
 
-function redToCreate(){
-    window.location.href = "./createCollection.php";
+function redToCreate() {
+  window.location.href = "./createCollection.php";
 }
-function redToDash(){
-    window.location.href = './dashboard.php'
+function redToDash() {
+  window.location.href = "./dashboard.php";
 }
